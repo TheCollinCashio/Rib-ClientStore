72bb3d9c0d5b5e88093229af93695d46bef48f07
Collin Cashio <collin.cashio@selu.edu>
2019-03-14 15:02:42 -0500

    working with Jake

--- src/RibStore.ts
+++ src/RibStore.ts
@@ -1,4 +1,4 @@
-export default class RibStore {
+export class ClientStore {
     private data = new Map<string, any>()
     private functionMap = new Map<string, ((value?: any) => void)[]>()
 
@@ -42,6 +42,76 @@ export default class RibStore {
             returnObj[key] = this.data.get(key) || obj[key]
         }
         return returnObj
+    }
+
+    private unBind(key: string, fnToUnbind: (value?: any) => void) {
+        const functions = this.functionMap.get(key)
+        this.functionMap.set(key, functions.filter(fn => fn !== fnToUnbind))
+    }
+}
+
+export class ServerStore {
+    private data = new Map<string, any>()
+    private functionMap = new Map<string, ((value?: any) => void)[]>()
+    private ribInstance : Rib
+    private isBound = false
+
+    constructor(obj: object) {
+        this.set(obj)
+    }
+
+    set(obj: object, addFunc?: (value?: any) => void) {
+        let unBindFunctions = []
+
+        for (let key in obj) {
+            this.data.set(key, obj[key])
+
+            let functions = this.functionMap.get(key)
+            
+            if (functions) {
+                functions.forEach(fn => fn({ [key]: obj[key] }))
+            }
+
+            if (addFunc) {
+                if (functions) {
+                    this.functionMap.set(key, [...functions, addFunc])
+                } else {
+                    this.functionMap.set(key, [addFunc])
+                }
+
+                unBindFunctions.push(() => {
+                    this.unBind(key, addFunc)
+                })
+            } 
+            
+            if (this.isBound) {
+                let ioFunction = () => {
+                    //  emit to all
+                }
+
+                if (functions) {
+                    this.functionMap.set(key, [...functions, ioFunction])
+                } else {
+                    this.functionMap.set(key, [ioFunction])
+                }
+            }
+        }
+
+        return () => {
+            unBindFunctions.forEach(f => f())
+        }
+    }
+
+    get(obj : object) {
+        let returnObj = {}
+        for (let key in obj) {
+            returnObj[key] = this.data.get(key) || obj[key]
+        }
+        return returnObj
+    }
+
+    exposeStore(ribInstance: any) {
+        
     }
 
     private unBind(key: string, fnToUnbind: (value?: any) => void) {

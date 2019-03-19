4d2993c6f2060c682518303e6e8a661fe7824b6a
Collin Cashio <collin.cashio@selu.edu>
2019-03-19 14:10:18 -0500

    this will be a realtime store on client which links to server

--- lib/RibStore.d.ts
+++ lib/RibStore.d.ts
@@ -1,8 +1,26 @@
-export default class RibStore {
+import RibServer from 'rib-server';
+import RibClient from 'rib-client';
+export declare class ClientStore {
     private data;
     private functionMap;
     constructor(obj: object);
     set(obj: object, addFunc?: (value?: any) => void): () => void;
     get(obj: object): {};
+    bindToServerStore(ribInstance: RibClient, serverName: string): void;
+    private unBind;
+}
+export declare class ServerStore {
+    private data;
+    private functionMap;
+    private availableSockets;
+    private ribInstance;
+    private storeName;
+    private isExposed;
+    private isPublicStore;
+    constructor(obj: object);
+    set(obj: object, addFunc?: (value?: any) => void): () => void;
+    get(obj: object): {};
+    exposeStore(storeName: string, ribInstance: RibServer, isPublicStore?: boolean): void;
+    giveAccess(client: any): void;
     private unBind;
 }
